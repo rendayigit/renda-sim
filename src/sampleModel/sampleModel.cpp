43b77df87b47054f61a6b1724ea8d606e8e04525
rendayigit <renda.yigit@gmail.com>
2024-11-21 08:00:33 +0300

    add more steps to sample model

--- src/sampleModel/sampleModel.cpp
+++ src/sampleModel/sampleModel.cpp
@@ -3,16 +3,41 @@
 #include "sampleModel.hpp"
 #include "services/eventManager/eventManager.hpp"
 
-constexpr double TIME_STEP = 1000; // 1 sec
+constexpr double TIME_STEP_1_SEC = 1000;
+constexpr double TIME_STEP_500_MSEC = 500;
+constexpr double TIME_STEP_100_MSEC = 100;
+constexpr double TIME_STEP_1_MSEC = 1;
 
-SampleModel::SampleModel() : m_event(new SimpleEvent) {
+SampleModel::SampleModel()
+    : m_eventSlow(new SimpleEvent), m_eventFast(new SimpleEvent), m_eventFaster(new SimpleEvent),
+      m_eventFastest(new SimpleEvent) {
   std::cout << "SampleModel Initialized" << std::endl;
 
-  m_event->setEventFunction([&] { step(); });
-  m_event->setCycleMillis(TIME_STEP);
-  m_event->activate();
+  m_eventSlow->setEventFunction([&] { step("1000 ms"); });
+  m_eventSlow->setCycleMillis(TIME_STEP_1_SEC);
+  m_eventSlow->activate();
 
-  EventManager::getInstance().addEvent(m_event);
+  m_eventFast->setEventFunction([&] { step("500 ms"); });
+  m_eventFast->setCycleMillis(TIME_STEP_500_MSEC);
+  m_eventFast->activate();
+
+  m_eventFaster->setEventFunction([&] { step("100 ms"); });
+  m_eventFaster->setCycleMillis(TIME_STEP_100_MSEC);
+  m_eventFaster->activate();
+
+  m_eventFastest->setEventFunction([&] { step("1 ms"); });
+  m_eventFastest->setCycleMillis(TIME_STEP_1_MSEC);
+  m_eventFastest->activate();
+
+  EventManager::getInstance().addEvent(m_eventSlow);
+  EventManager::getInstance().addEvent(m_eventFast);
+  EventManager::getInstance().addEvent(m_eventFaster);
+  EventManager::getInstance().addEvent(m_eventFastest);
 }
 
-void SampleModel::step() { std::cout << "SampleModel Step" << std::endl; }
+void SampleModel::step(const std::string &stepTime) {
+  auto now = std::chrono::steady_clock::now();
+  auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(now - m_appStartTime).count();
+
+  std::cout << stepTime << " Step " << std::to_string(milliseconds) << std::endl;
+}
--- src/sampleModel/sampleModel.hpp
+++ src/sampleModel/sampleModel.hpp
@@ -1,13 +1,20 @@
 #pragma once
 
+#include <chrono>
+
 #include "services/eventManager/eventManager.hpp"
 #include "services/eventManager/simpleEvent.hpp"
 
 class SampleModel {
 public:
   SampleModel();
-  static void step();
+  void step(const std::string &);
 
 private:
-  SimpleEvent *m_event;
+  SimpleEvent *m_eventSlow;
+  SimpleEvent *m_eventFast;
+  SimpleEvent *m_eventFaster;
+  SimpleEvent *m_eventFastest;
+
+  std::chrono::steady_clock::time_point m_appStartTime = std::chrono::steady_clock::now();
 };
