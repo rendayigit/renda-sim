ed5702ee84158562ed640587f447fd3ad419edcd
rendayigit <renda.yigit@gmail.com>
2024-11-22 20:03:12 +0300

    added logger tests

--- src/SourceFiles.cmake
+++ src/SourceFiles.cmake
@@ -1,12 +1,13 @@
 set(SOURCEFILES
-    # Model Source Files
     ${CMAKE_SOURCE_DIR}/src/main.cpp
+    # Service Source Files
     ${CMAKE_SOURCE_DIR}/src/services/scheduler/scheduler.cpp
     ${CMAKE_SOURCE_DIR}/src/services/eventManager/eventManager.cpp
     ${CMAKE_SOURCE_DIR}/src/services/logger/logger.cpp
     ${CMAKE_SOURCE_DIR}/src/services/json/json.cpp
     ${CMAKE_SOURCE_DIR}/src/services/network/client/client.cpp
     ${CMAKE_SOURCE_DIR}/src/services/network/server/server.cpp
+    # Model Source Files
     ${CMAKE_SOURCE_DIR}/src/pcs/pcs.cpp
     ${CMAKE_SOURCE_DIR}/src/sampleModel/sampleModel.cpp)
 
--- tests/TestFiles.cmake
+++ tests/TestFiles.cmake
@@ -1,14 +1,18 @@
 set(TESTFILES
+    # Source Files
     ${CMAKE_SOURCE_DIR}/src/services/scheduler/scheduler.cpp
     ${CMAKE_SOURCE_DIR}/src/services/eventManager/eventManager.cpp
     ${CMAKE_SOURCE_DIR}/src/services/logger/logger.cpp
+    ${CMAKE_SOURCE_DIR}/src/services/json/json.cpp
     ${CMAKE_SOURCE_DIR}/src/services/network/client/client.cpp
     ${CMAKE_SOURCE_DIR}/src/services/network/server/server.cpp
     ${CMAKE_SOURCE_DIR}/src/pcs/pcs.cpp
-    ${CMAKE_SOURCE_DIR}/src/services/json/json.cpp
+    # Test Files
+    ${CMAKE_SOURCE_DIR}/tests/services/logger/loggerTest.cpp
     ${CMAKE_SOURCE_DIR}/tests/services/json/jsonTest.cpp
     ${CMAKE_SOURCE_DIR}/tests/sampleTest.cpp
     ${CMAKE_SOURCE_DIR}/tests/eventManagerTest.cpp)
 
 set(INCLUDEDIRS
-    ${CMAKE_SOURCE_DIR}/src/)
+    ${CMAKE_SOURCE_DIR}/src/
+    ${CMAKE_SOURCE_DIR}/tests/)
