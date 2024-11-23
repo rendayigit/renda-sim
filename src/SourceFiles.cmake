ec4bd12a4c1296120fd15a56521d5fbf3666903e
rendayigit <renda.yigit@gmail.com>
2024-11-23 18:40:54 +0300

    fix cmakelist relative paths

--- src/SourceFiles.cmake
+++ src/SourceFiles.cmake
@@ -1,20 +1,20 @@
 set(SOURCEFILES
-    ${CMAKE_SOURCE_DIR}/src/main.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/../src/main.cpp
     # Common Source Files
-    ${CMAKE_SOURCE_DIR}/src/common/load/load.cpp
-    ${CMAKE_SOURCE_DIR}/src/common/load/loadState.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/../src/common/load/load.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/../src/common/load/loadState.cpp
     # Service Source Files
-    ${CMAKE_SOURCE_DIR}/src/services/eventManager/eventManager.cpp
-    ${CMAKE_SOURCE_DIR}/src/services/json/json.cpp
-    ${CMAKE_SOURCE_DIR}/src/services/logger/logger.cpp
-    ${CMAKE_SOURCE_DIR}/src/services/network/client/client.cpp
-    ${CMAKE_SOURCE_DIR}/src/services/network/server/server.cpp
-    ${CMAKE_SOURCE_DIR}/src/services/scheduler/scheduler.cpp
-    ${CMAKE_SOURCE_DIR}/src/services/timer/timer.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/../src/services/eventManager/eventManager.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/../src/services/json/json.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/../src/services/logger/logger.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/../src/services/network/client/client.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/../src/services/network/server/server.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/../src/services/scheduler/scheduler.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/../src/services/timer/timer.cpp
     # Model Source Files
-    ${CMAKE_SOURCE_DIR}/src/models/pcs/pcs.cpp
-    ${CMAKE_SOURCE_DIR}/src/models/sampleModel/sampleModel.cpp)
+    ${CMAKE_CURRENT_LIST_DIR}/../src/models/pcs/pcs.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/../src/models/sampleModel/sampleModel.cpp)
 
 set(INCLUDEDIRS
-    ${CMAKE_SOURCE_DIR}/src/
-    ${CMAKE_SOURCE_DIR}/src/models/)
+    ${CMAKE_CURRENT_LIST_DIR}/../src/
+    ${CMAKE_CURRENT_LIST_DIR}/../src/models/)
--- tests/TestFiles.cmake
+++ tests/TestFiles.cmake
@@ -1,34 +1,34 @@
 set(TESTFILES
     # Common Source Files
-    ${CMAKE_SOURCE_DIR}/src/common/load/load.cpp
-    ${CMAKE_SOURCE_DIR}/src/common/load/loadState.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/../src/common/load/load.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/../src/common/load/loadState.cpp
     # Service Source Files
-    ${CMAKE_SOURCE_DIR}/src/services/eventManager/eventManager.cpp
-    ${CMAKE_SOURCE_DIR}/src/services/json/json.cpp
-    ${CMAKE_SOURCE_DIR}/src/services/logger/logger.cpp
-    ${CMAKE_SOURCE_DIR}/src/services/network/client/client.cpp
-    ${CMAKE_SOURCE_DIR}/src/services/network/server/server.cpp
-    ${CMAKE_SOURCE_DIR}/src/services/scheduler/scheduler.cpp
-    ${CMAKE_SOURCE_DIR}/src/services/timer/timer.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/../src/services/eventManager/eventManager.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/../src/services/json/json.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/../src/services/logger/logger.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/../src/services/network/client/client.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/../src/services/network/server/server.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/../src/services/scheduler/scheduler.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/../src/services/timer/timer.cpp
     # Model Source Files
-    ${CMAKE_SOURCE_DIR}/src/models/pcs/pcs.cpp
-    ${CMAKE_SOURCE_DIR}/src/models/sampleModel/sampleModel.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/../src/models/pcs/pcs.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/../src/models/sampleModel/sampleModel.cpp
     # Common Test Files
-    ${CMAKE_SOURCE_DIR}/tests/common/loadTest.cpp
-    ${CMAKE_SOURCE_DIR}/tests/common/dataFlowTest.cpp
-    ${CMAKE_SOURCE_DIR}/tests/common/failureTest.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/../tests/common/loadTest.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/../tests/common/dataFlowTest.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/../tests/common/failureTest.cpp
     # Service Test Files
-    ${CMAKE_SOURCE_DIR}/tests/services/network/networkTest.cpp
-    ${CMAKE_SOURCE_DIR}/tests/services/eventManagerTest.cpp
-    ${CMAKE_SOURCE_DIR}/tests/services/simpleEventTest.cpp
-    ${CMAKE_SOURCE_DIR}/tests/services/jsonTest.cpp
-    ${CMAKE_SOURCE_DIR}/tests/services/loggerTest.cpp
-    ${CMAKE_SOURCE_DIR}/tests/services/fileOperationsTest.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/../tests/services/network/networkTest.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/../tests/services/eventManagerTest.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/../tests/services/simpleEventTest.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/../tests/services/jsonTest.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/../tests/services/loggerTest.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/../tests/services/fileOperationsTest.cpp
     # Model Test Files
-    # ${CMAKE_SOURCE_DIR}/tests/models/pcsTest.cpp
+    # ${CMAKE_CURRENT_LIST_DIR}/../tests/models/pcsTest.cpp
     )
 
 set(INCLUDEDIRS
-    ${CMAKE_SOURCE_DIR}/src/
-    ${CMAKE_SOURCE_DIR}/src/models/
-    ${CMAKE_SOURCE_DIR}/tests/)
+    ${CMAKE_CURRENT_LIST_DIR}/../src/
+    ${CMAKE_CURRENT_LIST_DIR}/../src/models/
+    ${CMAKE_CURRENT_LIST_DIR}/../tests/)
