fcbe101dbea5910dba4573390646b7f324ae99f4
rendayigit <renda.yigit@gmail.com>
2024-12-13 09:53:07 +0300

    implementing plotting using matplotlib.animation

--- ui/plot/plot.py
+++ ui/plot/plot.py
@@ -1,15 +1,16 @@
 import threading
 import time
-import matplotlib.pyplot as plt
 import math
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 
 class GenericPlotter:
     def __init__(self, title="Generic Plot", xlabel="X", ylabel="Y", max_data_points=100):
-        self.fig, self.ax = plt.subplots()
         self.xdata, self.ydata = [], []
         self.max_data_points = max_data_points
 
+        self.fig, self.ax = plt.subplots()
         self.title = title
         self.xlabel = xlabel
         self.ylabel = ylabel
@@ -19,19 +20,25 @@ class GenericPlotter:
         self.ax.set_ylabel(self.ylabel)
         self.ax.set_autoscaley_on(True)
 
-        # Create a line object and store it
-        self.line, = self.ax.plot([], [], 'ro-')
+        (self.line,) = self.ax.plot([], [], "ro-")
 
         self.time = 0
 
-    def update_plot(self, new_y):
+    def init_animation(self):
+        return (self.line,)
+
+    def update_plot(self, frame_data):
+        new_y = frame_data
+
         self.xdata.append(self.time)
         self.ydata.append(new_y)
 
+        self.time = self.time + 1
+
         # Limit data points to maintain performance
         if len(self.xdata) > self.max_data_points:
-            self.xdata = self.xdata[-self.max_data_points:]
-            self.ydata = self.ydata[-self.max_data_points:]
+            self.xdata = self.xdata[-self.max_data_points :]
+            self.ydata = self.ydata[-self.max_data_points :]
 
         # Update line data efficiently
         self.line.set_data(self.xdata, self.ydata)
@@ -44,26 +51,22 @@ class GenericPlotter:
         self.ax.set_xlim(xmin - 0.1 * x_range, xmax + 0.1 * x_range)
         self.ax.set_ylim(ymin - 0.1 * y_range, ymax + 0.1 * y_range)
 
-        self.fig.canvas.draw()
-        self.fig.canvas.flush_events()
-        self.time = self.time + 1  # TODO: increment with last sim time delta
+        return (self.line,)
 
-    def start_test_thread(self):
-        def test_thread_func():
-            x_val = 0
-            while True:
-                self.update_plot(math.sin(x_val))
-                x_val += 0.1
-                time.sleep(0.01)
-
-        test_thread = threading.Thread(target=test_thread_func)
-        test_thread.start()
-
-    def show(self):
+    def start_animation(self):
+        anim = animation.FuncAnimation(
+            self.fig, self.update_plot, frames=self.generate_data, init_func=self.init_animation, save_count=100, interval=100, blit=True
+        )  # Update every 100ms
         plt.show()
+
+    def generate_data(self):
+        x_val = 0
+        while True:
+            yield math.sin(x_val)
+            x_val += 0.1
+            time.sleep(0.01)  # Simulate data generation time
 
 
 if __name__ == "__main__":
     plotter = GenericPlotter(title="Real-time Plot", xlabel="Time (s)", ylabel="Value")
-    plotter.start_test_thread()
-    plotter.show()
+    plotter.start_animation()
