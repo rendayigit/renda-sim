1c8c3f9591dd05a0c23554e6b5bba9ecfa8d8282
rendayigit <renda.yigit@gmail.com>
2025-02-13 09:55:28 +0300

    minor script fix

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,9 +4,9 @@ SCRIPTDIR=`pwd`
 cd -
 
 # Build Engine
-cd $SCRIPTDIR/Engine
+cd $SCRIPTDIR/../Engine
 make -j$((`nproc`+2))
 
 # Build Models
-cd $SCRIPTDIR/Models
+cd $SCRIPTDIR/../Models
 make -j$((`nproc`+2))
